64ac91ddbf4bc67e44178102a58ea7d721bd32f5
Page Asgardius <asgardius@asgardius.company>
2022-08-27 16:48:02 -0700

    video player template

--- app/src/main/java/asgardius/page/s3manager/MainActivity.java
+++ app/src/main/java/asgardius/page/s3manager/MainActivity.java
@@ -2,7 +2,10 @@ package asgardius.page.s3manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //This is to launch video playback test
+        Button videotest = (Button)findViewById(R.id.vtest);
+        videotest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //buttonaction
+                videoplayer();
+            }
+        });
+    }
+
+    private void videoplayer() {
+
+        Intent intent = new Intent(this, VideoPlayer.class);
+
+        startActivity(intent);
+
     }
 }
