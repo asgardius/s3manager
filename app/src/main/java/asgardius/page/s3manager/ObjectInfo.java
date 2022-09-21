ac1858ba2b6ec1fb685ced7d392b6f22ad1d03f7
Page Asgardius <asgardius@asgardius.company>
2022-09-21 10:53:07 -0700

    object-info-interface

--- app/src/main/java/asgardius/page/s3manager/ObjectInfo.java
+++ app/src/main/java/asgardius/page/s3manager/ObjectInfo.java
@@ -15,6 +15,10 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectInfo extends AppCompatActivity {
     String username, password, endpoint, bucket, object, location;
@@ -23,7 +27,8 @@ public class ObjectInfo extends AppCompatActivity {
     AWSCredentials myCredentials;
     AmazonS3 s3client;
     ProgressBar simpleProgressBar;
-    TextView filesize;
+    TextView filesize, filesizeinfo;
+    boolean isobject, isfolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class ObjectInfo extends AppCompatActivity {
         setContentView(R.layout.activity_object_info);
         simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
         filesize = (TextView) findViewById(R.id.size);
+        filesizeinfo = (TextView) findViewById(R.id.size_info);
         endpoint = getIntent().getStringExtra("endpoint");
         username = getIntent().getStringExtra("username");
         password = getIntent().getStringExtra("password");
@@ -50,6 +56,55 @@ public class ObjectInfo extends AppCompatActivity {
             s3ClientOptions.setPathStyleAccess(true);
         }
         s3client.setS3ClientOptions(s3ClientOptions);
-        simpleProgressBar.setVisibility(View.INVISIBLE);
+        Thread getInfo = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    //Your code goes here
+                    if (object == null) {
+                        isobject = false;
+                    } else {
+                        isobject = true;
+                        if (object.endsWith("/")) {
+                            isfolder = true;
+                        } else {
+                            isfolder = false;
+                        }
+                    }
+
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (isobject) {
+                                if (isfolder) {
+                                    filesizeinfo.setText(getResources().getString(R.string.folder_size));
+                                } else {
+                                    filesizeinfo.setText(getResources().getString(R.string.file_size));
+                                }
+                            } else {
+                                filesizeinfo.setText(getResources().getString(R.string.bucket_size));
+                            }
+                            simpleProgressBar.setVisibility(View.INVISIBLE);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.media_list_fail), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    //Toast.makeText(getApplicationContext(),getResources().getString(R.string.media_list_fail), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+        getInfo.start();
     }
 }
