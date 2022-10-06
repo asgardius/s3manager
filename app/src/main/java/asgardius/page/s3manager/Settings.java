7bc22c5c7cdb14cc1aaf82e66bf1bec625174de3
Page Asgardius <asgardius@asgardius.company>
2022-10-06 08:01:48 -0700

    settings screen template

---
+++ app/src/main/java/asgardius/page/s3manager/Settings.java
@@ -0,0 +1,27 @@
+package asgardius.page.s3manager;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import asgardius.page.s3manager.databinding.ActivitySettingsBinding;
+
+public class Settings extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
