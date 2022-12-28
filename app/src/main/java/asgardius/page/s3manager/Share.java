0e62408f1d1e0d9f4227e6b67fe4cdb11567cea4
Page Asgardius <asgardius@asgardius.company>
2022-12-27 18:16:39 -0700

    new release

--- app/src/main/java/asgardius/page/s3manager/Share.java
+++ app/src/main/java/asgardius/page/s3manager/Share.java
@@ -30,9 +30,9 @@ public class Share extends AppCompatActivity {
     AWSCredentials myCredentials;
     AmazonS3 s3client;
     Calendar mycal;
-    EditText datepick, monthpick, yearpick, hourpick, minutepick;
-    int date, month, year, hour, minute;
-    Button share, external;
+    EditText datepick, hourpick, minutepick;
+    int date, hour, minute;
+    Button share;
     GeneratePresignedUrlRequest request;
     Date expiration;
     URL objectURL;
@@ -73,12 +73,9 @@ public class Share extends AppCompatActivity {
         setContentView(R.layout.activity_share);
         mycal = Calendar.getInstance();
         datepick = (EditText)findViewById(R.id.Date);
-        monthpick = (EditText)findViewById(R.id.Month);
-        yearpick = (EditText)findViewById(R.id.Year);
         hourpick = (EditText)findViewById(R.id.Hour);
         minutepick = (EditText)findViewById(R.id.Minute);
         share = (Button)findViewById(R.id.share);
-        external = (Button)findViewById(R.id.open_in);
         endpoint = getIntent().getStringExtra("endpoint");
         username = getIntent().getStringExtra("username");
         password = getIntent().getStringExtra("password");
@@ -106,64 +103,49 @@ public class Share extends AppCompatActivity {
             public void onClick(View view) {
                 //buttonaction
                 try {
-                    if (datepick.getText().toString().equals("") || monthpick.getText().toString().equals("") ||
-                            yearpick.getText().toString().equals("") || hourpick.getText().toString().equals("") ||
-                            minutepick.getText().toString().equals("")) {
-                        Toast.makeText(getApplicationContext(),getResources().getString(R.string.null_expiration_date), Toast.LENGTH_SHORT).show();
+                    if (datepick.getText().toString().equals("")) {
+                        date = 0;
                     } else {
                         date = Integer.parseInt(datepick.getText().toString());
-                        month = Integer.parseInt(monthpick.getText().toString());
-                        year = Integer.parseInt(yearpick.getText().toString());
-                        hour = Integer.parseInt(hourpick.getText().toString());
-                        minute = Integer.parseInt(minutepick.getText().toString());
-                        mycal.set(Calendar.YEAR, year);
-                        mycal.set(Calendar.MONTH, month-1);
-                        mycal.set(Calendar.DATE, date);
-                        mycal.set(Calendar.HOUR, hour);
-                        mycal.set(Calendar.MINUTE, minute);
-                        mycal.set(Calendar.SECOND, 0);
-                        expiration = mycal.getTime();
-                        //System.out.println(expiration);
-                        request = new GeneratePresignedUrlRequest(bucket, object).withExpiration(expiration);
-                        objectURL = s3client.generatePresignedUrl(request);
-                        //System.out.println(URLify(objectURL.toString()));
-                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                        shareIntent.setType("text/plain");
-                        shareIntent.putExtra(Intent.EXTRA_TEXT, URLify(objectURL.toString()));
-                        startActivity(Intent.createChooser(shareIntent, "choose one"));
                     }
-                    } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(),getResources().getString(R.string.invalid_expiration_date), Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-        });
-        external.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                //buttonaction
-                try {
-                    if (mediafile) {
-                        expiration = new Date();
-                        mycal.setTime(expiration);
-                        //System.out.println("today is " + mycal.getTime());
-                        mycal.add(Calendar.HOUR, videotime);
-                        //System.out.println("Expiration date: " + mycal.getTime());
-                        expiration = mycal.getTime();
-                        request = new GeneratePresignedUrlRequest(bucket, object).withExpiration(expiration);
+                    if (hourpick.getText().toString().equals("")) {
+                        hour = 0;
                     } else {
-                        request = new GeneratePresignedUrlRequest(bucket, object);
+                        hour = Integer.parseInt(hourpick.getText().toString());
                     }
+                    if (minutepick.getText().toString().equals("")) {
+                        minute = 0;
+                    } else {
+                        minute = Integer.parseInt(minutepick.getText().toString());
+                    }
+                    expiration = new Date();
+                    System.out.println("today is " + mycal.getTime());
+                    mycal.setTime(expiration);
+                    if (date == 0 && hour == 0 && minute == 0) {
+                        if (mediafile) {
+                            mycal.add(Calendar.HOUR, videotime);
+                        } else {
+                            mycal.add(Calendar.MINUTE, 15);
+                        }
+                    } else {
+                        mycal.add(Calendar.DATE, date);
+                        mycal.add(Calendar.HOUR, hour);
+                        mycal.add(Calendar.MINUTE, minute);
+                    }
+                    System.out.println("Expiration date: " + mycal.getTime());
+                    expiration = mycal.getTime();
+                    //System.out.println(expiration);
+                    request = new GeneratePresignedUrlRequest(bucket, object).withExpiration(expiration);
                     objectURL = s3client.generatePresignedUrl(request);
                     //System.out.println(URLify(objectURL.toString()));
                     Intent shareIntent = new Intent(Intent.ACTION_SEND);
                     shareIntent.setType("text/plain");
                     shareIntent.putExtra(Intent.EXTRA_TEXT, URLify(objectURL.toString()));
                     startActivity(Intent.createChooser(shareIntent, "choose one"));
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(),getResources().getString(R.string.invalid_expiration_date), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(),getResources().getString(R.string.invalid_expiration_date), Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
 
         });
     }
